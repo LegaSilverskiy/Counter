ff12e573b5cf472a436a435cf425ff799e2ed82f
LegaSilverskiy <slepchenko98@yandex.ru>
2023-09-21 22:51:57 +0500

    Перекрасил кнопку "+" в красный цвет Добавлена кнопка "-" для уменьшения счетчика на -1, если счетчик больше 0 (Цвет синий) Добавлена кнопка сброса, которая сбрасывает счетчик на 0 Добавлено текстовое поле для логирования нажатий на кнопки и фиксации времени

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,24 +8,60 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBOutlet var backViewColor: UIView!
-    @IBOutlet weak var countLabel: UILabel!
-    @IBOutlet weak var plusButton: UIButton!
-     var countNumber: Int = 0
+    
+    @IBOutlet var backViewColor: UIView! // Добавил и переименовал стандартный View для изменения фонового цвета, но по итогу передумал. Оставил для возможности все таки менять цвет
+    @IBOutlet weak var countLabel: UILabel! // лейбл для счетчика
+    @IBOutlet weak var plusButton: UIButton! // кнопка +1
+    @IBOutlet weak var minusButton: UIButton! // кнопка -1
+    @IBOutlet weak var resetButton: UIButton! // кнопка сброса
+    @IBOutlet weak var actionHistory: UITextView! // UITextView для логов
+    private var countNumber: Int = 0 // переменная для записи значений счетчика
+    private func currentDate () -> String { // функция для определения и форматирования даты
+        let dateFormatter = DateFormatter()
+        let date = Date()
+        dateFormatter.dateFormat = "dd.MM.yyyy, HH:mm:ss"
+        let newData = dateFormatter.string(from: date)
+        return newData
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        backViewColor.backgroundColor = .gray
-        countLabel.text = "Значение счётчика: \(countNumber)"
-        countLabel.backgroundColor = .black
-        countLabel.textColor = .white
-        plusButton.backgroundColor = .green
-        plusButton.tintColor = .white
+        
+        countLabel.text = "\(countNumber)" // установил в лейбл текст и переменную с счетчиком
+        countLabel.backgroundColor = .white // покрасил лейбл в белый
+        countLabel.textColor = .black // покрасил текст в лейбле в черный
+        plusButton.backgroundColor = .red // покрасил кнопку "+" в красный
+        plusButton.tintColor = .white // покрасил знак "+" в белый
+        minusButton.backgroundColor = .blue // покрасил кнопку "-" в синий
+        minusButton.tintColor = .white //покрасил знак "-" в белый
+        actionHistory.backgroundColor = .black // покрасил фон UItextView в черный
+        actionHistory.textColor = .white // покрасил текст в UItextView в белый
+        actionHistory.text = "История изменений: \n" // указал заголовок в UItextView
+        actionHistory.isEditable = false // запредил редактирование для пользователя в UItextView
         // Do any additional setup after loading the view.
     }
-
-    @IBAction func increaseButton(_ sender: Any) {
+    
+    @IBAction func increaseButton(_ sender: Any) { // +1 к счетчику и запись логов с датой
         countNumber += 1
         countLabel.text = "Значение счётчика: \(countNumber)"
+        
+        actionHistory.insertText("\(currentDate()) значение изменено на +1 \n")
+        
+    }
+    @IBAction func decreaseButton(_ sender: Any) { // -1 к счетчику и запись логов с датой
+        if countNumber > 0 {
+            countNumber -= 1
+            actionHistory.insertText("\(currentDate()) значение изменено на -1 \n")
+        }
+        else{
+            actionHistory.insertText("\(currentDate()) попытка уменьшить значение счётчика ниже 0 \n")
+        }
+        countLabel.text = "Значение счётчика: \(countNumber)"
+    }
+    @IBAction func resetButton(_ sender: Any) { // сброс счетчика до 0 и запись логов с датой
+        countNumber = 0
+        countLabel.text = "Значение счётчика: \(countNumber)"
+        actionHistory.insertText("\(currentDate()) значение сброшено \n")
+        
     }
 }
