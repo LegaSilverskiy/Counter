f11d124154c51b58f49f608bbfb10fc3c70b986b
LegaSilverskiy <slepchenko98@yandex.ru>
2023-09-21 19:56:59 +0500

    Добавлены лейбл, куда записывается значение счетчика, и кнопка увеличения счетчика. Так же добавлен экшн для увеличения счетчика по нажатию на кноку на +1

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var backViewColor: UIView!
+    @IBOutlet weak var countLabel: UILabel!
+    @IBOutlet weak var plusButton: UIButton!
+     var countNumber: Int = 0
     override func viewDidLoad() {
         super.viewDidLoad()
+        backViewColor.backgroundColor = .gray
+        countLabel.text = "Значение счётчика: \(countNumber)"
+        countLabel.backgroundColor = .black
+        countLabel.textColor = .white
+        plusButton.backgroundColor = .green
+        plusButton.tintColor = .white
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func increaseButton(_ sender: Any) {
+        countNumber += 1
+        countLabel.text = "Значение счётчика: \(countNumber)"
+    }
 }
-
